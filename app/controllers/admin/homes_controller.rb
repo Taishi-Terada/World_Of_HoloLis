c24091b9e7f16ccd37dce9fb428d6a8961302835
EC2 Default User <ec2-user@ip-172-31-33-56.ap-northeast-1.compute.internal>
2023-09-07 17:29:59 +0000

    管理者のトップページ作成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,7 @@
 class Admin::HomesController < ApplicationController
   def top
+    @vtuber_informations = VtuberInformation.all
+    @new_news = News.new
+    @news = News.all.page(params[:page]).per(8)
   end
 end
--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,7 +1,19 @@
 class Admin::NewsController < ApplicationController
   def create
+    @news = News.new(news_params)
+    @news.save
+    redirect_to admin_root_path(@news), notice: "投稿が完了しました"
   end
 
   def destroy
+    news = News.find(params[:id])
+    news.destroy
+    redirect_to admin_root_path, notice: "投稿を削除しました"
+  end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:news_name, :news_introduction, :created_at, :updated_at)
   end
 end

8e7971fde248c5ef2135fffb2b23e51cffe82ca6
EC2 Default User <ec2-user@ip-172-31-33-56.ap-northeast-1.compute.internal>
2023-09-07 11:59:32 +0000

    会員のトップページ作成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @vtuber_informations = VtuberInformation.all
+    @news = News.all.page(params[:page]).per(8)
   end
 
   def about
